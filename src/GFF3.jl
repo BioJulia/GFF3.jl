13df1f3d9e943c5e63e4a7a0a5daabb82742c28f
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2020-02-02 17:42:45 +1100

    Naive code roundup

--- src/GFF3.jl
+++ src/GFF3.jl
@@ -1,5 +1,20 @@
+# GFF3 File Format
+# ================
+
 module GFF3
 
-greet() = print("Hello World!")
+import Automa
+import Automa.RegExp: @re_str
+import BGZFStreams
+import BioCore.Exceptions: missingerror
+import BioSequences
+import BufferedStreams
+import GenomicFeatures: GenomicFeatures, Interval, IntervalCollection
+import URIParser
+using BioCore
+
+include("record.jl")
+include("reader.jl")
+include("writer.jl")
 
 end # module
